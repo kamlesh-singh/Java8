8d5ca9a6b3780229eda05ee3e58635f38342928d
kamlesh-singh <114075275+kamlesh-singh@users.noreply.github.com>
2022-10-04 09:33:23 +0530

    Update Ans4.java

--- Ans4.java
+++ Ans4.java
@@ -37,6 +37,29 @@ class Employee{
     public void setCity(String city) {
         this.city = city;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee other = (Employee) obj;
+        String name1= this.fullName.split(" ")[0];
+        String name2= other.fullName.split(" ")[0];
+
+        return Objects.equals(name1, name2);
+    }
+    @Override
+    public int hashCode() {
+        String name = this.fullName.split(" ")[0];
+        return Objects.hash(name);
+    }
     @Override
     public String toString(){
         return getFullName();
@@ -55,11 +78,15 @@ public class Ans4 {
 
    List<Employee> empList = new ArrayList<>();
    empList.add(new Employee("kamlesh singh",30000L,"haldwani"));
+
    empList.add(new Employee("himanshu nayal",2000L,"delhi"));
+   empList.add(new Employee("himanshu joshi",1000L,"delhi"));
+
    empList.add(new Employee("abhinav kamboj",10000L,"delhi"));
+
    empList.add(new Employee("rajat gupta",200L,"delhi"));
    empList.add(new Employee("ujjwal gupta",21000L,"bhimtal"));
-   empList.add(new Employee("sam bisht" ,40000L,"haldwani"));
+   empList.add(new Employee("sam bisht" ,40000L,"delhi"));
 
         Predicate<Employee> predicate = (e) -> {
             if(e.getSalary() < 5000 && e.getCity().equals("delhi")){
@@ -67,7 +94,7 @@ public class Ans4 {
             }
             return false;
         };
-    empList.stream().filter(predicate).forEach(e-> System.out.println(e));
+    empList.stream().distinct().filter(predicate).forEach(System.out::println);
 
 
 
